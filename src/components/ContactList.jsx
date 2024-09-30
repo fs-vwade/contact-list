8de3f91fb4ff0d4122b6920a380e6f87c9f7b6ec
Victor Wade <p2525657888@gmail.com>
2024-09-29 22:59:10 -1000

    api connection, established

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Contact from "./Contact.jsx";
+
+const API_URL = "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com";
 
 const dummyContacts = [
 	{ id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -9,6 +11,19 @@ const dummyContacts = [
 
 export default function ContactList() {
 	const [contacts, setContacts] = useState(dummyContacts);
+
+	useEffect(() => {
+		const getContacts = async () => {
+			try {
+				const response = await fetch(`${API_URL}/users`);
+				const result = await response.json();
+				setContacts(result);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		getContacts();
+	}, []);
 
 	return (
 		<table>
