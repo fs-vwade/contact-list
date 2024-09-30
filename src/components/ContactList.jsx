0b9b9afcc59584fbda78a8805a32a5640b09915e
Victor Wade <p2525657888@gmail.com>
2024-09-29 22:37:50 -1000

    dummy contacts

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,4 +1,14 @@
-export default function Contact({ name, email, phone }) {
+import { useState } from "react";
+
+const dummyContacts = [
+	{ id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+	{ id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+	{ id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
+
+export default function ContactList() {
+	const [contacts, setContacts] = useState(dummyContacts);
+
 	return (
 		<table>
 			<thead>
@@ -12,6 +22,9 @@ export default function Contact({ name, email, phone }) {
 					<td>Email</td>
 					<td>Phone</td>
 				</tr>
+				{
+					// map data
+				}
 			</tbody>
 		</table>
 	);
